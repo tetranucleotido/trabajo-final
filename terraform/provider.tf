87acd2bf475a0f69e8cb7d0495289a40835c4945
tetranucleotido <lmmonteronaveda@gmail.com>
2023-02-10 19:49:05 -0300

    update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "${var.app}-s3-tfstate"
+    bucket = "tetra-s3-tfstate"
     key = "global/s3/terraform.tfstate"
     region = "us-east-1"
     
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "${var.app}-s3-tfstate"
+  bucket = "tetra-s3-tfstate"
 
   lifecycle {
     prevent_destroy = true
