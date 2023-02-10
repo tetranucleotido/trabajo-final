e423221dab48fe224010cb14c6e7ff69c0c5ac4c
tetranucleotido <lmmonteronaveda@gmail.com>
2023-02-10 19:46:25 -0300

    update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,26 @@
+terraform {
+  backend "s3" {
+    bucket = "${var.app}-s3-tfstate"
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-1"
+    
+  }
+
+}
+
 provider "aws" {
     region = "us-east-1"
   
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "${var.app}-s3-tfstate"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
 }
