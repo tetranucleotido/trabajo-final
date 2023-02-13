0790455f8b4f76a896fb770db13229f91d95a1c5
Tetranucleotido <78046615+tetranucleotido@users.noreply.github.com>
2023-02-13 10:14:28 -0300

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,20 +1,11 @@
-locals {
-  common_tags = {
-    Terraform = "true"
-    Enviroment = var.env
-    Owner = "Tetra DevOps"
-    Project = "${var.app}-devops"   
-  }
-}
 
 variable "app" {}
 
 variable "env" {}
-
-### variables de EC2
-
 variable "ami" {}
 
 variable "instance_type" {}
 
-variable "key" {}
+variable "key" {
+    default = "temporal"
+}
