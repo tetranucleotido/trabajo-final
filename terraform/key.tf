5f22cd25838b50373441f3539236c365e07a451f
tetranucleotido <lmmonteronaveda@gmail.com>
2023-02-13 14:57:10 -0300

    update

--- terraform/key.tf
+++ terraform/key.tf
@@ -4,17 +4,21 @@
 #    public_key = file("keys/aws_terraform.pub")
 #}
 
-resource "aws_key_pair" "TF_key" {
-  key_name   = "TF_key"
-  public_key = tls_private_key.rsa.public_key_openssh
-}
-
 resource "tls_private_key" "rsa" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
+resource "aws_key_pair" "TF_key" {
+  key_name   = "TF_key"
+  public_key = tls_private_key.rsa.public_key_openssh
+}
+
+
+
+#Crear .pem local
 resource "local_file" "TF-key" {
   content  = tls_private_key.rsa.private_key_pem
   filename = "tfkey"
 }
+
--- terraform/output.tf
+++ terraform/output.tf
@@ -1,6 +1,11 @@
 output "dns_publica_server" {
-    description = "DNS publica del servidor 1"
+    description = "IP Publica servidor Develop"
     value = "http://${aws_instance.amazonlinux.public_ip}"
 }
 
 
+output "access_key" {
+    description = "Key pair"
+    value = tls_private_key.rsa.private_key_pem
+
+}
