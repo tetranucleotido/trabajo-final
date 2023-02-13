70980e956bf1b76526e4f50fb3aa8cf7728a56cc
Tetranucleotido <78046615+tetranucleotido@users.noreply.github.com>
2023-02-13 10:16:06 -0300

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,5 +1,9 @@
-output "ec2_ip" {
-    description = "IP publica de la Instancia EC2"
-    value = aws_instance.amazonlinux.public_ip
+output "dns_publica_server" {
+    description = "DNS publica del servidor 1"
+    value = "http://${aws_instance.server.public_dns}"
+}
+output "ip_publica_server" {
+  description = "Ip Publica bastion"
+  value = "ssh -i temporal.pem ec2-user@${aws_instance.server.public_ip}"
 }
 
