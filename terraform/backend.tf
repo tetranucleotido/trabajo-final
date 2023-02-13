003bc4d61e24abfbadf723980c7ca2645cd1f588
tetranucleotido <lmmonteronaveda@gmail.com>
2023-02-13 12:59:11 -0300

    update

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,12 +1,12 @@
-# #Create s3 bucket
-# resource "aws_s3_bucket" "terraform_state" {
-#   bucket = "tetra-s3-tfstate"
+#Create s3 bucket
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "tetra-s3-tfstate"
 
-#   lifecycle {
-#     prevent_destroy = true
-#   }
+  lifecycle {
+    prevent_destroy = true
+  }
 
-#   versioning {
-#     enabled = true
-#   }
-# }
+  versioning {
+    enabled = true
+  }
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,12 @@
 
-terraform {
-  backend "s3" {
-    bucket = "tetra-s3-tfstate"
-    key = "global/mystatefile/terraform.tfstate"
-    region = "us-east-1"
+# terraform {
+#   backend "s3" {
+#     bucket = "tetra-s3-tfstate"
+#     key = "global/mystatefile/terraform.tfstate"
+#     region = "us-east-1"
     
-  }
-}
+#   }
+# }
 provider "aws" {
     region = "us-east-1"
   
