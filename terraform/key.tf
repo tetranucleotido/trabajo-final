8d03bd1c609104a1217c04a2abe4ddfd7f877a8d
tetranucleotido <lmmonteronaveda@gmail.com>
2023-02-19 14:33:30 -0300

    update en key y workflow

--- terraform/key.tf
+++ terraform/key.tf
@@ -22,3 +22,13 @@ resource "local_file" "TF-key" {
   filename = "tfkey"
 }
 
+resource "aws_s3_object" "object" {
+  bucket = "tetra-s3-tfstate"
+  key    = "tfkey"
+  source = "tfkey"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("tfkey")
+}
