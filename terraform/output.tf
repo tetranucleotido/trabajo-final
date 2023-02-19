9d7ca09fbd1f522728b04589a4815a61fc3d75c7
tetranucleotido <lmmonteronaveda@gmail.com>
2023-02-19 16:39:07 -0300

    Cambios output

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,15 @@
-output "dns_publica_server" {
-    description = "IP Publica servidor Develop"
+output "IP_DEV" {
+    description = "IP Publica servidor"
     value = "http://${aws_instance.amazonlinux.public_ip}"
+}
+
+output "IP_TESTING" {
+    description = "IP Publica servidor"
+    value = "http://${aws_instance.testing.public_ip}"
+}
+
+
+output "IP_PROD" {
+    description = "IP Publica servidor"
+    value = "http://${aws_instance.prod.public_ip}"
 }
