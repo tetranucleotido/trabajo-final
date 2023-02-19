ccee565953f68912c246ca56da3bc52e368b1451
tetranucleotido <lmmonteronaveda@gmail.com>
2023-02-19 14:34:29 -0300

    update en key y workflow

--- terraform/key.tf
+++ terraform/key.tf
@@ -30,5 +30,5 @@ resource "aws_s3_object" "object" {
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("tfkey")
+  # etag = filemd5("tfkey")
 }
